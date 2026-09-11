5b54193497190ac6bd43e3c997d482adf2e8e760
aastha-vij <vijaastha96@gmail.com>
2026-09-11 22:12:53 +0530

    complete cart page automation suite

--- pages/CartPage.js
+++ pages/CartPage.js
@@ -2,8 +2,38 @@ export class CartPage {
     constructor(page) {
         this.page = page;
         this.cartTitle = page.getByText('Your Cart');
-        this.backpack = page.locator('.cart_item').filter({
-            hasText: 'Sauce Labs Backpack',
+        this.cartItem = page.locator('.cart_item');
+    }
+
+    getCartItems() {
+        return this.cartItem;
+    }
+
+    getProduct(productName) {
+        return this.cartItem.filter({
+            hasText: productName,
         });
+    }
+
+    getProductPrice(productName) {
+        return this.getProduct(productName)
+            .locator('.inventory_item_price');
+    }
+
+    getProductQuantity(productName) {
+        return this.getProduct(productName)
+            .locator('.cart_quantity');
+    }
+
+    getRemoveBtn(productName) {
+        return this.getProduct(productName)
+            .getByRole('button', { name: 'Remove' });
+    }
+    async isCartPageVisible() {
+        return await this.cartTitle.isVisible();
+    }
+
+    async removeProduct(productName) {
+        await this.getRemoveBtn(productName).click();
     }
 }
--- tests/smoke.spec.js
+++ tests/smoke.spec.js
@@ -6,31 +6,6 @@ import { users } from '../testData/users';
 import { ProductDetailsPage } from '../pages/ProductDetailsPage';
 
 const user = users.standard;
-test('User can add a product to the cart @smoke @cart', async ({ page }) => {
-
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  const cartPage = new CartPage(page);
-
-  await page.goto('/');
-  await expect(page).toHaveTitle('Swag Labs');
-  await loginPage.login(user.username, user.password);
-  await expect(page).toHaveURL(/.*inventory.html/);
-
-  await expect(inventoryPage.productsTitle).toBeVisible();
-
-  await inventoryPage.addProductToCart('Sauce Labs Backpack');
-  expect(await inventoryPage.isRemoveButtonVisible('Sauce Labs Backpack')).toBe(true);
-  await expect(inventoryPage.getProductName('Sauce Labs Backpack')).toHaveText('Sauce Labs Backpack');
-  await expect(inventoryPage.getProductPrice('Sauce Labs Backpack')).toHaveText('$29.99');
-
-  await expect(inventoryPage.shoppingCartBadge).toHaveText('1');
-  await inventoryPage.goToCart();
-  await expect(page).toHaveURL(/.*cart.html/);
-
-  await expect(cartPage.cartTitle).toBeVisible();
-  await expect(cartPage.backpack).toBeVisible();
-});
 
 test('User can add multiple products to the cart @smoke @products', async ({ page }) => {
 
@@ -114,4 +89,169 @@ test('User can verify product details @smoke @products', async ({ page }) => {
 
   await expect(productDetailsPage.productPrice)
     .toHaveText('$29.99');
+});
+
+test('User can add a product to the cart @smoke @cart', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
+
+  await page.goto('/');
+  await expect(page).toHaveTitle('Swag Labs');
+  await loginPage.login(user.username, user.password);
+  await expect(page).toHaveURL(/.*inventory.html/);
+
+  await expect(inventoryPage.productsTitle).toBeVisible();
+
+  await inventoryPage.addProductToCart('Sauce Labs Backpack');
+  expect(await inventoryPage.isRemoveButtonVisible('Sauce Labs Backpack')).toBe(true);
+  await expect(inventoryPage.getProductName('Sauce Labs Backpack')).toHaveText('Sauce Labs Backpack');
+  await expect(inventoryPage.getProductPrice('Sauce Labs Backpack')).toHaveText('$29.99');
+
+  await inventoryPage.goToCart();
+  await expect(page).toHaveURL(/.*cart.html/);
+
+  await expect(cartPage.cartTitle).toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).toBeVisible();
+
+  await expect(
+    cartPage.getProductPrice('Sauce Labs Backpack')
+  ).toHaveText('$29.99');
+
+  await expect(
+    cartPage.getProductQuantity('Sauce Labs Backpack')
+  ).toHaveText('1');
+});
+
+test('User can verify multiple products in cart @smoke @cart', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
+
+  await page.goto('/');
+  await loginPage.login(user.username, user.password);
+
+  await expect(inventoryPage.productsTitle).toBeVisible();
+
+  await inventoryPage.addProductToCart('Sauce Labs Backpack');
+  await inventoryPage.addProductToCart('Sauce Labs Bike Light');
+
+  await inventoryPage.goToCart();
+
+  await expect(page).toHaveURL(/.*cart.html/);
+  await expect(cartPage.cartTitle).toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Bike Light')
+  ).toBeVisible();
+
+  await expect(
+    cartPage.getProductPrice('Sauce Labs Backpack')
+  ).toHaveText('$29.99');
+
+  await expect(
+    cartPage.getProductPrice('Sauce Labs Bike Light')
+  ).toHaveText('$9.99');
+
+  await expect(
+    cartPage.getProductQuantity('Sauce Labs Backpack')
+  ).toHaveText('1');
+
+  await expect(
+    cartPage.getProductQuantity('Sauce Labs Bike Light')
+  ).toHaveText('1');
+});
+
+test('Cart displays correct number of products @smoke @cart', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
+
+  await page.goto('/');
+  await loginPage.login(user.username, user.password);
+
+  await expect(inventoryPage.productsTitle).toBeVisible();
+
+  await inventoryPage.addProductToCart('Sauce Labs Backpack');
+  await inventoryPage.addProductToCart('Sauce Labs Bike Light');
+
+  await inventoryPage.goToCart();
+
+  await expect(cartPage.cartTitle).toBeVisible();
+
+  await expect(cartPage.getCartItems()).toHaveCount(2);
+});
+
+test('User can remove a product from cart @smoke @cart', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
+
+  await page.goto('/');
+  await loginPage.login(user.username, user.password);
+
+  await expect(inventoryPage.productsTitle).toBeVisible();
+
+  await inventoryPage.addProductToCart('Sauce Labs Backpack');
+  await inventoryPage.addProductToCart('Sauce Labs Bike Light');
+
+  await inventoryPage.goToCart();
+
+  await expect(cartPage.cartTitle).toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).toBeVisible();
+
+  await cartPage.removeProduct('Sauce Labs Backpack');
+  await expect(cartPage.getCartItems()).toHaveCount(1);
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).not.toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Bike Light')
+  ).toBeVisible();
+});
+
+test('Cart becomes empty after removing the only product @smoke @cart', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const inventoryPage = new InventoryPage(page);
+  const cartPage = new CartPage(page);
+
+  await page.goto('/');
+  await loginPage.login(user.username, user.password);
+
+  await expect(inventoryPage.productsTitle).toBeVisible();
+
+  await inventoryPage.addProductToCart('Sauce Labs Backpack');
+
+  await inventoryPage.goToCart();
+
+  await expect(cartPage.cartTitle).toBeVisible();
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).toBeVisible();
+
+  await cartPage.removeProduct('Sauce Labs Backpack');
+
+  await expect(
+    cartPage.getProduct('Sauce Labs Backpack')
+  ).not.toBeVisible();
+
+  await expect(cartPage.getCartItems()).toHaveCount(0);
 });
